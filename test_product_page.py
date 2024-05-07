c8c59916ce0c8b22f11ce349e36d1cdc9059032b
RustamPenza <97059623+RustamPenza@users.noreply.github.com>
2024-05-07 22:14:15 +0300

    Добавлены отрицательные тесты: 1)test_guest_cant_see_success_message_after_adding_product_to_basket 2) test_message_disappeared_after_adding_product_to_basket Добавлен тест для проверки отсутствия элемента: 1)test_guest_cant_see_success_message

--- test_product_page.py
+++ test_product_page.py
@@ -21,3 +21,26 @@ def test_guest_can_add_product_in_basket(browser, link):
     name = browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
     price = browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
     page.should_be_add_to_basket(name, price)
+
+
+@pytest.mark.skip
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message_after_adding_product_to_basket()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_not_be_success_message()
+
+
+@pytest.mark.xfail
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_disappeared_after_adding_product_to_basket()
