18e7c45f6bb2f6f10289ebe70763fdd9b26210c7
RustamPenza <97059623+RustamPenza@users.noreply.github.com>
2024-05-07 22:46:41 +0300

    Добавлены тесты страницы авторизации и отображения кнопки "Войти/Зарегистрироваться"

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 from .pages.locators import ProductPageLocators
 
 
@@ -44,3 +45,19 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     page = ProductPage(browser, link)
     page.open()
     page.should_disappeared_after_adding_product_to_basket()
+
+
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
