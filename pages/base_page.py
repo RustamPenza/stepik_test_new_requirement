83ca628c4a1c62159740c6d392fd7d84de4243fd
RustamPenza <97059623+RustamPenza@users.noreply.github.com>
2024-05-07 22:11:39 +0300

    Добавлены методы: 1) is_disappeared 2) is_not_element_present

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,10 @@
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.common.exceptions import TimeoutException
 import math
+
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
@@ -12,6 +16,7 @@ class BasePage:
     def open(self):
         self.browser.get(self.url)
 
+    # Проверяем, что элемент отображается на странице
     def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
@@ -19,6 +24,24 @@ class BasePage:
             return False
         return True
 
+    # Проверяем, что элемент исчезает, с использованием явного ожидания
+    def is_disappeared(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException). \
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+        return True
+
+    # Проверяем, что элемент не появляется на странице в течение заданного времени
+    def is_not_element_present(self, how, what, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+        return False
+
+    # Метод для работы с алертом
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
