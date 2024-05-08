84cd5f78537ad3aa92bdfb1c08a60dc7cbb68bf7
RustamPenza <97059623+RustamPenza@users.noreply.github.com>
2024-05-08 20:37:40 +0300

    Добавлена функция регистрации пользователя

--- pages/login_page.py
+++ pages/login_page.py
@@ -3,6 +3,17 @@ from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
+
+    def register_new_user(self, email, password):
+        email_input = self.browser.find_element(*LoginPageLocators.INPUT_EMAIL)
+        email_input.send_keys(email)
+        password1_input = self.browser.find_element(*LoginPageLocators.INPUT_PASSWORD1)
+        password1_input.send_keys(password)
+        password2_input = self.browser.find_element(*LoginPageLocators.INPUT_PASSWORD2)
+        password2_input.send_keys(password)
+        button = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
+        button.click()
+
     def should_be_login_page(self):
         self.should_be_login_url()
         self.should_be_login_form()
