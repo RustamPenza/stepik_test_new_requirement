808a2266f36bad819333325ef0f555708629d992
RustamPenza <97059623+RustamPenza@users.noreply.github.com>
2024-05-08 20:39:12 +0300

    Объединил тесты связанные с авторизованным пользователем в один класс

--- test_product_page.py
+++ test_product_page.py
@@ -1,8 +1,40 @@
 import pytest
+import time
+import random
 
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.locators import ProductPageLocators
+from .pages.base_page import BasePage
+
+SYMBOLS = list('123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM')
+
+
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        email = str(time.time()) + "@fakemail.org"
+        password = ''.join([random.choice(SYMBOLS) for x in range(9)])
+        link = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        page = LoginPage(browser, link)
+        page.open()
+        page.register_new_user(email, password)
+        base_page = BasePage(browser, browser.current_url)
+        base_page.should_be_authorized_user()
+
+    def test_user_can_add_product_in_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        name = browser.find_element(*ProductPageLocators.NAME_PRODUCT).text
+        price = browser.find_element(*ProductPageLocators.PRICE_PRODUCT).text
+        page.should_be_add_to_basket(name, price)
+
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0"
+        page = ProductPage(browser, link)
+        page.open()
+        page.should_not_be_success_message()
 
 
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
